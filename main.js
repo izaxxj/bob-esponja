a36ba4e6fb06fbda96dc64364bfc2e59e8cefd2e
izaxxj <162719073+izaxxj@users.noreply.github.com>
2024-03-12 11:23:50 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://criadoresid.com/wp-content/uploads/2020/10/bob-esponja-em-portugues.jpg";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://img.olhardigital.com.br/wp-content/uploads/2021/03/Bob-Esponja.jpg";
+}
